24dad1ae8f6e914de856c590c303823fb1d25eca
AHMATOLOG <murtaz.oziev@mail.ru>
2022-02-23 15:38:05 +0300

    пуш

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create();
-
+ 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = localStorage.token;
   return config;
